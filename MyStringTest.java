2f3d260a8d91cafe06bea07a0a66d5e1d07437b9
Mike Stinson <mstinson@Mikes-MacBook-Pro.local>
2014-10-01 02:05:21 -0400

    first commit

---
+++ MyStringTest.java
@@ -0,0 +1,170 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MyStringTest {
+
+	//fail("Not yet implemented");
+	
+	@Test
+	public void testCharAtValid() {
+		
+	}
+
+	@Test
+	public void testCharAtOutOfBoundsPositive() {
+		
+	}
+	
+	@Test
+	public void testCharAtOutOfBoundsNegative() {
+		
+	}
+
+	@Test
+	public void testCharAtWhitespace() {
+		
+	}
+	
+	@Test
+	public void testContainsCharFound() {
+		
+	}
+	
+	@Test
+	public void testContainsCharNotFound() {
+		
+	}
+	
+	@Test
+	public void testContainsCharIgnoreCaseFound() {
+		
+	}
+	
+	@Test
+	public void testContainsCharIgnoreCaseNotFound() {
+		
+	}
+	
+	@Test
+	public void testCopy() {
+		
+	}
+	
+	@Test
+	public void testStarsWithCharFound() {
+		
+	}
+	
+	@Test
+	public void testStartsWithCharNotFound() {
+		
+	}
+	
+	@Test
+	public void testEndsWithCharFound() {
+		
+	}
+	
+	@Test
+	public void testEndsWithCharNotFound() {
+		
+	}
+	
+	@Test
+	public void testFirstIndexOfFound() {
+		
+	}
+	
+	@Test
+	public void testFirstIndexOfNotFound() {
+		
+	}
+	
+	@Test
+	public void testLastIndexOfFound() {
+		
+	}
+	
+	@Test
+	public void testLastIndexOfNotFound() {
+		
+	}
+	
+	@Test
+	public void testIndexOfFromFound() {
+		
+	}
+	
+	@Test
+	public void testIndexOfFromNotFound() {
+		
+	}
+	
+	@Test
+	public void testToCharArray() {
+		
+	}
+	
+	@Test
+	public void testLength() {
+		
+	}
+	
+	@Test
+	public void testReplaceFirstInstanceCharFound() {
+		
+	}
+	
+	@Test
+	public void testReplaceFirstInstanceCharNotFound() {
+		
+	}
+	
+	@Test
+	public void testReplaceLastInstanceCharFound() {
+		
+	}
+	
+	@Test
+	public void testReplaceLastInstanceCharNotFound() {
+		
+	}
+	
+	@Test
+	public void testReplaceAllCharFound() {
+		
+	}
+	
+	@Test
+	public void testReplaceAllCharNotFound() {
+		
+	}
+	
+	@Test
+	public void testAccessToUppercaseMyString() {
+		
+	}
+	
+	@Test
+	public void testAccessToUppercaseMyStringAlreadyUppercase() {
+		
+	}
+	
+	@Test
+	public void testAccessToLowercaseMyString() {
+		
+	}
+	
+	@Test
+	public void testAccessToLowercaseMyStringAlreadyUppercase() {
+		
+	}
+	
+	
+	@Test
+	public void test() {
+		
+	}
+}

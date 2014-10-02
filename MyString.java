e9c29b0e50327dd92a0d40c369d8ff591f236845
rob brown <rab170@pitt.edu>
2014-10-02 01:41:33 -0400

    added comments for MyString Class

--- MyString.java
+++ MyString.java
@@ -1,3 +1,8 @@
+//  This is a simple String class that is initialized based on in input character array (char[] chars)
+//
+//  There are various methods associated with the MyString class, of which descriptions can be found
+//  below. Most important is likely the toString function which converts the input to a string.
+
 
 public class MyString {
     char [] chars;
@@ -7,6 +12,9 @@ public class MyString {
     }
 
     public char charAt (int i) {
+        //  INPUT: integer index
+        //  OUTPUT: the character at that index, if exists
+
         if (i > 0 && i < chars.length)
             return chars[i];
         else
@@ -14,6 +22,9 @@ public class MyString {
     }
 
     public boolean containsChar (char c) {
+        //  INPUT:  a characater "char c"
+        //  OUTPUT: true if the character is in char[] chars, false otherwise
+
         for (char i : chars) {
             if (i == c) return true;
         }
@@ -21,7 +32,10 @@ public class MyString {
     }
 
     public boolean containsCharIgnoreCase (char c) {
-    	for (char i : chars) {
+        //  INPUT:  a characater "char c"
+        //  OUTPUT: true if the character is in char[] chars (irrespective of case), false otherwise
+
+        for (char i : chars) {
             if (Character.toLowerCase(i) == Character.toLowerCase(c))
                 return true;
         }
@@ -29,20 +43,32 @@ public class MyString {
     }
 
     public MyString copy () {
+        //  INPUT:  N/A
+        //  OUTPUT: a new (shallow) copy of the MyString class
+
         return new MyString (chars);
     }
 
     public boolean startsWithChar (char c) {
+        //  INPUT:  a character "char c" 
+        //  OUTPUT: true if the first index of char[] chars is c, false otherwise
+
         if (chars[0] == c) return true;
         return false;
     }
 
     public boolean endsWithChar (char c) {
+        //  INPUT:  a character "char c" 
+        //  OUTPUT: true if the last index of char[] chars is c, false otherwise
+
         if (chars[chars.length-1] == c) return true;
         return false;
     }
 
     public int firstIndexOf (char c) {
+        //  INPUT:  a character (char c)
+        //  OUTPUT: the index of the first occurrence of c in char[] chars 
+
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == c) return i;
         }
@@ -50,6 +76,9 @@ public class MyString {
     }
 
     public int lastIndexOf (char c) {
+        //  INPUT:  a character (char c)
+        //  OUTPUT: the index of the last occurrence of c in char[] chars 
+
         for (int i = chars.length - 1; i >= 0; i--) {
             if (chars[i] == c) return i;
         }
@@ -57,6 +86,9 @@ public class MyString {
     }
 
     public int indexOfFrom (char c, int j) {
+        //  INPUT:  character to look for (char c) and index to start at (int j)
+        //  OUTPUT: index of first match to c (where index >= j), -1 if no match is found
+
         for (int i = j; i < chars.length; i++){
             if (chars[i] == c) return i;
         }
@@ -64,14 +96,24 @@ public class MyString {
     }
 
     public char [] toCharArray () {
+        //  INPUT:  N/A
+        //  OUTPUT: the character array representation of the string in MyString 
+
         return chars;
     }
 
     public int length () {
+        //  INPUT:  N/A
+        //  OUTPUT: The length of the string
+
         return chars.length;
     }
 
     public MyString replaceFirstInstance (char toBeReplaced, char changeTo) {
+        //  INPUT:  Two characters; one to replace, one to be replaced
+        //  OUTPUT: The string-version of the char[] chars member of MyString, with the first instance of
+        //          "char toBeReplaced" replaced by "char changeTo"
+
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == toBeReplaced) {
                 chars[i] = changeTo;
@@ -82,6 +124,10 @@ public class MyString {
     }
 
     public MyString replaceLastInstance (char toBeReplaced, char changeTo) {
+        //  INPUT:  Two characters; one to replace, one to be replaced
+        //  OUTPUT: The string-version of the char[] chars member of MyString, with the last instance of
+        //          "char toBeReplaced" replaced by "char changeTo"
+
         for (int i = chars.length-1; i >= 0; i--) {
             if (chars[i] == toBeReplaced) {
                 chars[i] = changeTo;
@@ -92,6 +138,10 @@ public class MyString {
     }
 
     public MyString replaceAll (char toBeReplaced, char changeTo) {
+        //  INPUT:  Two characters; one to replace, one to be replaced
+        //  OUTPUT: The string-version of the char[] chars member of MyString, with all instances of 
+        //          "char toBeReplaced" replaced by "char changeTo"
+
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == toBeReplaced) {
                 chars[i] = changeTo;
@@ -101,6 +151,9 @@ public class MyString {
     }
 
     public MyString accessToUppercaseMyString () {
+        //  INPUT:  N/A
+        //  OUTPUT: char[] chars, converted to an all upper-case string
+
         char [] ret = chars;
         for (int i = 0; i < chars.length; i++) {
             ret[i] = Character.toUpperCase(chars[i]);
@@ -109,6 +162,9 @@ public class MyString {
     }
 
     public MyString accessToLowercaseMyString () {
+        //  INPUT:  N/A
+        //  OUTPUT: char[] chars, converted to an all lower-case string
+
         char [] ret = chars;
         for (int i = 0; i < chars.length; i++) {
             ret[i] = Character.toLowerCase(chars[i]);
@@ -117,19 +173,32 @@ public class MyString {
     }
 
     public void mutateToUppercaseMyString () {
+        //  INPUT: N/A
+        //  OUTPUT: N/A
+        //
+        //  This function modifies the char[] chars member of MyString to be upper-case.
+
         for (int i = 0; i < chars.length; i++) {
             chars[i] = Character.toUpperCase(chars[i]);
         }
     }
 
     public void mutateToLowercaseMyString () {
+        //  INPUT: N/A
+        //  OUTPUT: N/A
+        //
+        //  This function modifies the char[] chars member of MyString to be lowercase.
+
         for (int i = 0; i < chars.length; i++) {
             chars[i] = Character.toLowerCase(chars[i]);
         }
     }
 
     public char [] accessToUppercaseChars () {
-        char [] ret = chars;
+        //  INPUT:  N/A
+        //  OUTPUT: The char[] chars member of MyString, converted to upper-case
+
+       char [] ret = chars;
         for (int i = 0; i < chars.length; i++) {
             ret[i] = Character.toUpperCase(chars[i]);
         }
@@ -137,6 +206,9 @@ public class MyString {
     }
 
     public char [] accessToLowercaseChars () {
+        //  INPUT:  N/A
+        //  OUTPUT: The char[] chars member of MyString, converted to lower-case
+
         char [] ret = chars;
         for (int i = 0; i < chars.length; i++) {
             ret[i] = Character.toLowerCase(chars[i]);
@@ -145,46 +217,59 @@ public class MyString {
     }
 
     public void mutateToUppercaseChars () {
+        //  INPUT: N/A
+        //  OUTPUT: N/A
+        //
+        //  This function modifies the char[] chars member of MyString to be upper-case.
+
         for (int i = 0; i < chars.length; i++) {
             chars[i] = Character.toUpperCase(chars[i]);
         }
     }
 
     public void mutateToLowercaseChars () {
+        //  INPUT: N/A
+        //  OUTPUT: N/A
+        //
+        //  This function modifies the char[] chars member of MyString to be lowercase.
+
         for (int i = 0; i < chars.length; i++) {
             chars[i] = Character.toLowerCase(chars[i]);
         }
     }
 
     public String toString () {
+        //  INPUT: N/A
+        //  OUTPUT: the character array as a string
+
         return new String (chars);
     }
-    
+
     public boolean equals (MyString m) {
-    	// Return false if MyStrings are not same length
-    	if (this.length() != m.length()) return false;
-    	
-    	// Otherwise, test char arrays for equality
-    	char [] thisChars = this.toCharArray();
-    	char [] mChars = m.toCharArray();
-    	for (int i = 0; i < this.length(); i++) {
-    		if (thisChars[i] != mChars[i]) return false;
-    	}
-    	
-    	return true;
+        // Return false if MyStrings are not same length
+        if (this.length() != m.length()) return false;
+
+        // Otherwise, test char arrays for equality
+        char [] thisChars = this.toCharArray();
+        char [] mChars = m.toCharArray();
+        for (int i = 0; i < this.length(); i++) {
+            if (thisChars[i] != mChars[i]) return false;
+        }
+
+        return true;
     }
-    
+
     public boolean equalsIgnoreCase (MyString m) {
-    	// Return false if MyStrings are not same length
-    	if (this.length() != m.length()) return false;
-    	
-    	// Otherwise, test char arrays for equality
-    	char [] thisChars = this.toCharArray();
-    	char [] mChars = m.toCharArray();
-    	for (int i = 0; i < this.length(); i++) {
-    		if (Character.toLowerCase(thisChars[i]) != Character.toLowerCase(mChars[i])) return false;
-    	}
-    	
-    	return true;
+        // Return false if MyStrings are not same length
+        if (this.length() != m.length()) return false;
+
+        // Otherwise, test char arrays for equality
+        char [] thisChars = this.toCharArray();
+        char [] mChars = m.toCharArray();
+        for (int i = 0; i < this.length(); i++) {
+            if (Character.toLowerCase(thisChars[i]) != Character.toLowerCase(mChars[i])) return false;
+        }
+
+        return true;
     }
 }
